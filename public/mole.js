b69253f3ab30e09d6850936b7ff6cfae81feefb1
PlamenCode <plamenggg233@gmail.com>
2024-01-22 18:01:41 +0200

    firebase host
    host the game in firebase

--- public/mole.js
+++ public/mole.js
@@ -1,7 +1,42 @@
 let currentMoleTile;
 let currentPlantTile;
 let score = 0;
-let gameOver = false;  
+let gameOver = false;
+let isClicked = false;
+
+let moleTime = 1000;
+let plantTime = 2000;
+
+
+let easyBtn = document.getElementsByClassName('easy-btn');
+    easyBtn[0].addEventListener('click', () => {
+        moleTime = 2000;
+        plantTime = 3000;
+
+        easyBtn[0].classList = 'level-btn easy-btn selected';
+        normalBtn[0].classList = 'level-btn easy-btn';
+        hardBtn[0].classList = 'level-btn easy-btn';
+    })
+
+let normalBtn = document.getElementsByClassName('normal-btn');
+    normalBtn[0].addEventListener('click', () => {
+        moleTime = 1000;
+        plantTime = 2000;
+
+        easyBtn[0].classList = 'level-btn easy-btn';
+        normalBtn[0].classList = 'level-btn easy-btn selected';
+        hardBtn[0].classList = 'level-btn easy-btn';
+    })
+
+let hardBtn = document.getElementsByClassName('hard-btn');
+    hardBtn[0].addEventListener('click', () => {
+        moleTime = 500;
+        plantTime = 1000;
+
+        easyBtn[0].classList = 'level-btn easy-btn';
+        normalBtn[0].classList = 'level-btn easy-btn';
+        hardBtn[0].classList = 'level-btn easy-btn selected';
+    })
 
 let board = document.getElementById('board');
 let restartBtn = document.getElementById('restart-btn');
@@ -9,7 +44,6 @@ let restartBtn = document.getElementById('restart-btn');
 
 let startBtn = document.getElementById('start-btn');
     startBtn.addEventListener('click', () => { setGame() });
-
 
 
 function setGame(){
@@ -22,8 +56,8 @@ function setGame(){
     restartBtn.style.display = 'none';
     startBtn.style.display = 'none';
 
-    window.moleInterval = setInterval(setMole, 500);
-    window.plantInterval = setInterval(setPlant, 1000);
+    window.moleInterval = setInterval(setMole, moleTime);
+    window.plantInterval = setInterval(setPlant, plantTime);
 
 };
 
@@ -36,7 +70,7 @@ function restart(){
     setGame();
 };
 
-function getRnadomTime(){
+function getRnadomTile(){
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
@@ -51,11 +85,12 @@ function setMole(){
     let mole = document.createElement('img');
     mole.src = './assets/monty-mole.png'
 
-    let num = getRnadomTime();
+    let num = getRnadomTile();
     if(currentPlantTile && currentPlantTile.id == num) return;
 
     currentMoleTile = document.getElementById(num);
     currentMoleTile.appendChild(mole)
+    isClicked = false;
 };
 
 function setPlant(){
@@ -68,7 +103,7 @@ function setPlant(){
     let plant = document.createElement('img');
     plant.src = './assets/piranha-plant.png';
 
-    let num = getRnadomTime();
+    let num = getRnadomTile();
     if(currentMoleTile && currentMoleTile.id == num) return;
 
     currentPlantTile = document.getElementById(num);
@@ -78,9 +113,11 @@ function setPlant(){
 function selectTile(){
     if(gameOver) return;
 
-    if(this == currentMoleTile){
+    if(this == currentMoleTile && !isClicked){
         score += 10;
         document.getElementById('score').innerText = score.toString();
+        currentMoleTile.innerHTML = '';
+        isClicked = true;
     } else if (this == currentPlantTile){
         document.getElementById('score').innerText = `Game Over: ${score.toString()}`;
         restartBtn.style.display = 'block';
